5214612b57fa45d6247e2eaa7a6cdbbb06ed1956
PLKeluj <bulkazmaslem9@gmail.com>
2025-04-09 09:23:11 +0200

    Add files via upload

---
+++ 2017 czerwiec/ODP/Zadanie4/2017czerwiec_4_1.py
@@ -0,0 +1,31 @@
+def otworz_plik():
+    plik = open("punkty.txt", "r")
+    wspolrzedne = [[0] * 2 for j in range(len(plik.readlines()))]
+    i = 0
+    plik = open("punkty.txt", "r")
+    for liczba in plik:
+        liczba1, liczba2 = liczba.split(' ')
+        wspolrzedne[i][0] = int(liczba1.strip())
+        wspolrzedne[i][1] = int(liczba2.strip())
+        i += 1
+    return wspolrzedne
+
+
+def czy_pierwsza(liczba):
+    for i in range(2, liczba):
+        if liczba % i == 0:
+            return False
+    return True
+
+
+wspolrzedne = otworz_plik()
+
+wynik = 0
+
+for i in range(len(wspolrzedne)):
+    x = czy_pierwsza(wspolrzedne[i][0])
+    y = czy_pierwsza(wspolrzedne[i][1])
+    if x == True and y == True:
+        wynik += 1
+
+print(wynik)
