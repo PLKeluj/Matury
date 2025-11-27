ce29ca98ae7882842fe72cc40da13f21cf0221cf
PLKeluj <bulkazmaslem9@gmail.com>
2025-11-27 16:14:17 +0100

    2020

--- 2025 Informator/ODP/Zadanie1/Zadanie_1_4.py
+++ 2025 Informator/ODP/Zadanie1/Zadanie_1_4.py
@@ -1,8 +1,10 @@
-plik = open('dane1_3.txt', 'r')
+plik = open('dane1_4.txt', 'r')
 liczby = []
 
 for i in plik:
     liczby.append(int(i.strip()))
+
+# liczby = [2, -3, 1, -7, 4, -2, -1, 5, -3, 2, -1]
 
 sumy = [0] * len(liczby)
 
@@ -18,10 +20,16 @@ for i in range(len(liczby)):
         else:
             sumy[i][j - i] = sumy[i][j - i - 1] + liczby[j]
 
+print(sumy)
 max = 0
+id_p = 0
+id_k = 0
+
 for i in range(len(sumy)):
     for j in range(len(sumy[i])):
         if sumy[i][j] > max:
             max = sumy[i][j]
+            id_p = i
+            id_k = i + j
 
-print(max)
+print(id_p + 1, id_k + 1)
